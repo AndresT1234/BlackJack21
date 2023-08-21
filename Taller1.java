4a3c067ca5674509cd03a395348aa3f1a1013c9a
AndresT1234 <116287875+AndresT1234@users.noreply.github.com>
2023-08-21 14:50:07 -0500

    Revert "nuvo"
    This reverts commit a836fd75a78d3256d0daa7a390e1f162da6e71e0.

--- Taller1.java
+++ Taller1.java
@@ -42,12 +42,6 @@ public class Taller1 {
                 ronda++;
                 StdOut.println("\nRONDA NUMERO:    " + ronda + "\n");
                 for (Mano manolo : manitos) {
-
-                    if (manolo.operacionValor() > 0 && manolo.operacionValor() <= 21) { // Verificar si el jugador aún
-                                                                                        // puede jugar
-                        StdOut.println("\nJugador número " + manolo.getNum());
-                    }
-
                     StdOut.println("\nPara pedir ingrese 1, para pasar ingrese 0 ");
                     do {
                         pide = StdIn.readInt();
@@ -65,11 +59,6 @@ public class Taller1 {
                     } else
                         todosPasan++;
 
-                    if (manolo.operacionValor() > 21) {
-                        StdOut.println("Ya te pasaste de 21. Pasando al siguiente jugador.");
-
-                        continue; // Pasar al siguiente jugador si ya se pasó de 21
-                    }
                 }
             }
             byte count = 0;
