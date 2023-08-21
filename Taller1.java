bd58b997d1e015ee3467643a40b6ec868f3ec621
Emanuel Rios <emanuel.riosb@upb.edu.co>
2023-08-21 14:21:32 -0500

    Update Taller1.java

--- Taller1.java
+++ Taller1.java
@@ -1,7 +1,5 @@
 package upb.BlackJack21;
 
-import java.util.List;
-
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -13,39 +11,35 @@ public class Taller1 {
         int jugadores = StdIn.readByte();
 
         Baraja barajita = new Baraja();
-
         barajita.barajar();
 
-        LS_Black<Mano> manitos = new LS_Black<>();      
+        LS_Black<Mano> manitos = new LS_Black<>();
 
-        for(byte i = 1; i <= jugadores;i++){
+        for (byte i = 1; i <= jugadores; i++) {
             Mano manita = new Mano(i);
             manitos.addFinal(manita);
         }
 
-        if (jugadores > 0 && jugadores < 8) {
-
+        if (jugadores > 1 && jugadores < 8) {
+            byte pide;
             Carta card = new Carta();
             byte todosPasan = 0;
             byte ronda = 1;
-            StdOut.println("RONDA NUMERO:    " + ronda+"\n");
-            for (Mano manolo:manitos){
-                StdOut.println("EL JUGADOR NUMERO : "+ manolo.getNum()+" OBTUVO: \n");
+            StdOut.println("\nRONDA NUMERO:    " + ronda + "\n");
+            for (Mano manolo : manitos) {
+                StdOut.println("\nEL JUGADOR NUMERO : " + manolo.getNum() + " OBTUVO: ");
+                for (int i = 0; i < 2; i++) {
                     card = barajita.sacarCarta();
                     manolo.agregarCarta(card);
-                    StdOut.println(card.toString()+"\n");
-                    card = barajita.sacarCarta();
-                    manolo.agregarCarta(card);
-                    StdOut.println(card.toString()+"\n");
-
+                    StdOut.println(card.toString() + "");
+                }
             }
+
             while (todosPasan < jugadores) {
                 boolean verifica = false;
-                int pide ;
                 ronda++;
                 StdOut.println("RONDA NUMERO:    " + ronda+"\n");
                 for (Mano manolo:manitos){
-                    if(!manolo.getPasa()){
                     StdOut.println("Para pedir ingrese 1 para pasar ingrese 0");
                     do{
                         pide  = StdIn.readByte();
@@ -54,31 +48,28 @@ public class Taller1 {
 
                     }while(!verifica);
                     
-                    if(pide == 1 && manolo.operacionValor() <=21 ){
+                    if(pide == 1 && !manolo.getPasa()){
                         card = barajita.sacarCarta();
                         manolo.agregarCarta(card);
-                        StdOut.println("\n"+card.toString()+"\n");
-                    }
-                    else {
+                        StdOut.println("\n" + card.toString() + "\n");
+                    } else
                         todosPasan++;
                         manolo.pasa(true);
-                    }
-                }
 
                 }
             }
             byte count = 0;
             Mano[] arreglito = new Mano[jugadores];
-            for (Mano manolo:manitos){
-                arreglito[count] = manolo;;
-                count++;  
+            for (Mano manolo : manitos) {
+                arreglito[count] = manolo;
+                count++;
             }
             count = 0;
             int mayor = 0;
             Mano actual;
-            for (int i=0;i<arreglito.length;i++){
+            for (int i = 0; i < arreglito.length; i++) {
                 actual = arreglito[i];
-                if(actual.operacionValor()>mayor && actual.operacionValor()<= 21){
+                if (actual.operacionValor() > mayor && actual.operacionValor() <= 21) {
                     mayor = actual.operacionValor();
                 }
 
@@ -86,32 +77,29 @@ public class Taller1 {
             boolean empate = false;
             actual = null;
             LS_Black<Mano> winners = new LS_Black<Mano>();
-            for(int j=0;j<arreglito.length;j++){
+            for (int j = 0; j < arreglito.length; j++) {
                 actual = arreglito[j];
-                if(actual.operacionValor() == mayor){
+                if (actual.operacionValor() == mayor) {
                     winners.addFinal(actual);
                     count++;
                 }
-                if(count > 1) empate = true;
+                if (count > 1)
+                    empate = true;
             }
 
-            if(empate){
+            if (empate) {
                 StdOut.println("Los ganadores fueron:");
-                for(Mano manita: winners){
-                    StdOut.println("El jugador numero:"+manita.getNum()+" con puntaje: "+ manita.operacionValor());
+                for (Mano manita : winners) {
+                    StdOut.println("El jugador numero:" + manita.getNum() + " con puntaje: " + manita.operacionValor());
                 }
 
-            }
-            else  {
-                for(Mano manita2 : winners){
-                    StdOut.println("\nEl ganador fue:"+"\n"+"\n"+"    El jugador numero: "+manita2.getNum()+" con puntaje: "+ manita2.operacionValor()+
-                    "\n");
+            } else {
+                for (Mano manita2 : winners) {
+                    StdOut.println("\nEl ganador fue:" + "\n" + "\n" + "    El jugador numero: " + manita2.getNum()
+                            + " con puntaje: " + manita2.operacionValor() +
+                            "\n");
                 }
             }
-
-                                  
-
-    
 
         } else {
             StdOut.println(
